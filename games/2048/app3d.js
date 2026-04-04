1034eeaa6335f62395e9f3fa0e9b1dce69b76e84
rovelast <rover95rx@gmail.com>
2026-04-04 12:47:13 +0800

    修复 2048 静态页面模块加载

--- games/2048/app3d.js
+++ games/2048/app3d.js
@@ -1,4 +1,3 @@
-import "./style.css";
 import * as THREE from "three";
 import * as CANNON from "cannon-es";
 import { RoundedBoxGeometry } from "three/examples/jsm/geometries/RoundedBoxGeometry.js";
